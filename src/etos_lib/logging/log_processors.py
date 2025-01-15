567c18480751ab95bb68d2e3f6d8344a2f871838
Fredrik Fristedt <fredrik.fristedt@axis.com>
2025-01-15 10:45:06 +0100

    Update src/etos_lib/logging/log_processors.py
    Co-authored-by: Tobias Persson <tobiaspn@axis.com>

--- src/etos_lib/logging/log_processors.py
+++ src/etos_lib/logging/log_processors.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Custom log precessors for use with Open Telemetry logging signals."""
+"""Custom log processors for use with Open Telemetry logging signals."""
 
 from opentelemetry.sdk._logs import LogData, LogRecordProcessor
 
